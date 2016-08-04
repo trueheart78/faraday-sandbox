ecfa91a73abe6f6a8786bc6c484d95d802c457ae
Josh M <josh@trueheart78.com>
2016-08-04 01:06:55 -0400

    Spacing fix

--- get-example.rb
+++ get-example.rb
@@ -4,4 +4,4 @@ puts '** Basic GET **'
 
 stub_request(:any, "www.example.com").to_return(status: 200)
 
- Net::HTTP.get("www.example.com", "/")    # ===> Success
+Net::HTTP.get("www.example.com", "/")    # ===> Success
